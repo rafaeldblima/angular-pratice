b5c618a6fb6f4761f30e6cdfa3343f26a77e1db0
Rafael Lima <rafaeldblima@gmail.com>
2018-01-05 17:25:12 -0400

    Basic project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule
