1199928ee0aaa400c27df097e2e637b6b9e6e0d6
Rafael Lima <rafaeldblima@gmail.com>
2018-02-19 22:10:37 -0400

    Core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipeService } from './recipes/recipe.services';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpModule } from '@angular/http';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth.guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -28,14 +20,8 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     SharedModule,
     ShoppingListModule,
-    AuthModule
-  ],
-  providers: [
-    RecipeService,
-    DataStorageService,
-    AuthService,
-    AuthGuard,
-    ShoppingListService
+    AuthModule,
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
